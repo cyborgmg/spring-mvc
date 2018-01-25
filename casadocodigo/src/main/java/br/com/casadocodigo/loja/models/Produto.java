983792164f5faefcd0aaf58474ef7b6ec8ea9d4b
Unknown <cyborgmg@gmail.com>
2018-01-25 13:37:37 -0200

    25-jan-2018

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.models;
 
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,7 +16,8 @@ public class Produto {
     private String titulo;
     private String descricao;
     private int paginas;
-
+    @ElementCollection
+    private List<Preco> precos;
 
     public String getTitulo() {
         return titulo;
@@ -33,8 +37,20 @@ public class Produto {
     public void setPaginas(int paginas) {
         this.paginas = paginas;
     }
-
-    @Override
+    
+    public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public List<Preco> getPrecos() {
+		return precos;
+	}
+	public void setPrecos(List<Preco> precos) {
+		this.precos = precos;
+	}
+	@Override
     public String toString() {
         return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
     }
